1250b00848bf8e58cbf43cbb55b69256238277d9
quatro00 <josecarlosgarciadiaz@gmail.com>
2023-10-12 16:31:16 -0600

    actualizacion para subir archivos

--- Tickets.API/Controllers/TicketController.cs
+++ Tickets.API/Controllers/TicketController.cs
@@ -13,12 +13,38 @@ namespace Tickets.API.Controllers
     {
         [HttpPost, DisableRequestSizeLimit]
         [Authorize]
-        public async Task<IActionResult> Create()
+        public async Task<IActionResult> UploadFile()
         {
             //var response = await categoriaRepository.CreateAsync(request);
-            var formCollection = await Request.ReadFormAsync();
-            var file = formCollection.Files.First();
-            return Ok(Guid.NewGuid());
+            //var formCollection = await Request.ReadFormAsync();
+            //var file = formCollection.Files.First();
+            //return Ok(Guid.NewGuid());
+
+            try
+            {
+                var files = Request.Form.Files;
+                var folderName = Path.Combine("StaticFiles", "Images");
+                var pathToSave = Path.Combine(Directory.GetCurrentDirectory(), folderName);
+                if (files.Any(f => f.Length == 0))
+                {
+                    return BadRequest();
+                }
+                foreach (var file in files)
+                {
+                    var fileName = ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName.Trim('"');
+                    var fullPath = Path.Combine(pathToSave, fileName);
+                    var dbPath = Path.Combine(folderName, fileName); //you can add this path to a list and then return all dbPaths to the client if require
+                    using (var stream = new FileStream(fullPath, FileMode.Create))
+                    {
+                        file.CopyTo(stream);
+                    }
+                }
+                return Ok("All the files are successfully uploaded.");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "Internal server error");
+            }
         }
     }
 }
